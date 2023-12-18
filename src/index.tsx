4e7aff04a962637562ba4f0ffd7c72f3a2186da8
Dustin Irving <dustin.irving@gmail.com>
2023-12-17 22:05:20 -0500

    chore: minor config changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css';
 import './tailwind.css';
 
 const container = document.getElementById('root');
--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: '/portfolio-v2/',
+  base: '/',
 });
